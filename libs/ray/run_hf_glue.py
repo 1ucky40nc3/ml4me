381f89d0033ca2a116c6eee6e69096dde03deaad
1ucky40nc3 <louisnwendler@gmail.com>
2022-11-28 22:20:14 +0100

    added license

--- libs/ray/run_hf_glue.py
+++ libs/ray/run_hf_glue.py
@@ -1,3 +1,23 @@
+# Copyright 2022 Louis Wendler. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# 
+# Parts of the following code are copied/adapted from:
+# https://github.com/huggingface/transformers/blob/main/examples/pytorch/text-classification/run_glue.py
+# The original Apache License, Version 2.0 applies in theese cases.
+# Thanks to the HuggingFace Inc. team for their generous contribution.
+''' Finetune transformer models on the GLUE dataset '''
+
 from typing import (
     Any,
     Dict,
